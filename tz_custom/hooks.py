c6b690e15e8a732602a6d698e10a3c20f6959b6e
ibrahim-softsuave <ibrahim.softsuave@gmail.com>
2023-06-08 13:30:16 +0000

    hooks changes and customizeform changes

--- tz_custom/hooks.py
+++ tz_custom/hooks.py
@@ -94,9 +94,9 @@ app_license = "MIT"
 # ---------------
 # Override standard doctype classes
 
-# override_doctype_class = {
-#	"ToDo": "custom_app.overrides.CustomToDo"
-# }
+override_doctype_class = {
+	"Customize Form" : "tz_custom.custom.doctype.customize_form.customize_form.CustomCustomizeForm",
+}
 
 # Document Events
 # ---------------
